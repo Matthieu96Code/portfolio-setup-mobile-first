0d769b4e9388164d096f7957feffe02bd77d4f3d
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-01-31 08:01:13 +0000

    acitve event on menuList

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 let allBody = document.querySelector('body');
 let btn = document.querySelector('.nav-btn');
 let btnClose = document.querySelector('.close-btn');
+let menuList = document.querySelectorAll('.nav-toggle a');
 
 btn.addEventListener('click', function() {
-    allBody.classList.toggle('expand');
+    allBody.classList.add('expand');
 });
 
 btnClose.addEventListener('click', function() {
-    allBody.classList.toggle('expand');
+    allBody.classList.remove('expand');
 });
+
+for (let list of menuList) {
+    list.addEventListener('click', function() {
+        allBody.classList.remove('expand');
+    });    
+}
