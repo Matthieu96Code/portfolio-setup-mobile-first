1828ed7dad1d50f5faec3c5eee6a4bf31ce4a9e5
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-01-31 10:34:01 +0000

    fix es linters error #1

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-let allBody = document.querySelector('body');
-let btn = document.querySelector('.nav-btn');
-let btnClose = document.querySelector('.close-btn');
-let menuList = document.querySelectorAll('.nav-toggle a');
+const allBody = document.querySelector('body');
+const btn = document.querySelector('.nav-btn');
+const btnClose = document.querySelector('.close-btn');
+const menuList = document.querySelectorAll('.nav-toggle a');
 
-btn.addEventListener('click', function() {
-    allBody.classList.add('expand');
+btn.addEventListener('click', function addClassToBody () {
+  allBody.classList.add('expand');
 });
 
-btnClose.addEventListener('click', function() {
-    allBody.classList.remove('expand');
+btnClose.addEventListener('click', function removeClassToBody () {
+  allBody.classList.remove('expand');
 });
 
 for (let list of menuList) {
-    list.addEventListener('click', function() {
-        allBody.classList.remove('expand');
-    });    
+  list.addEventListener('click', function removeClassExpand () {
+    allBody.classList.remove('expand');
+  });
 }
