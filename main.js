34386eb82523fffb18b35b08430deaacac92711f
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-01-31 03:32:51 +0000

    Add the main JS file and link it to index.html

---
+++ main.js
@@ -0,0 +1,6 @@
+let allBody = document.querySelector('body');
+let btn = document.querySelector('.union-vecteur');
+
+allBody.addEventListener('click', function() {
+    allBody.classList.toggle('expand');
+});
