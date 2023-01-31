1cfcd3e602b586de7cbb8bc3cbfdaf37b6f21c67
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-01-31 07:12:58 +0000

    fix styling on css

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 let allBody = document.querySelector('body');
-let btn = document.querySelector('.union-vecteur');
+let btn = document.querySelector('.nav-btn');
+let btnClose = document.querySelector('.close-btn');
 
-allBody.addEventListener('click', function() {
+btn.addEventListener('click', function() {
+    allBody.classList.toggle('expand');
+});
+
+btnClose.addEventListener('click', function() {
     allBody.classList.toggle('expand');
 });
